2ebe5e23b30344584a220ab1bd2a0e046981571b
wjfatuan <wilson.forero@uan.edu.co>
2023-02-17 18:49:33 -0500

    Se agrega un TextView para mostrar el resultado, y se simplifica el codigo usando un solo metodo para manejar el clic

--- app/src/main/java/com/example/demo0212/MainActivity.kt
+++ app/src/main/java/com/example/demo0212/MainActivity.kt
@@ -3,16 +3,42 @@ package com.example.demo0212
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    var leftNumber = (0..100).random()
-    var rightNumber = (0+Math.random()*100).toInt()
+    var leftNumber = 0
+    var rightNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        assignNumbers()
+    }
+
+    fun assignNumbers() {
+        leftNumber = (0..100).random()
+        rightNumber = (0+Math.random()*100).toInt()
+    }
+
+    fun buttonClick(button: View) {
+        println("El numero derecho es $rightNumber y el izquierdo es $leftNumber")
+        println("El usuario dio click en el boton ${button.id}")
+        val result = findViewById<TextView>(R.id.txtResult)
+        if(button.id == R.id.btnRight && rightNumber>leftNumber) {
+            Toast.makeText(this, "Has ganado!", Toast.LENGTH_LONG).show()
+            result.text = "Has ganado!!"
+        }
+        else if(button.id == R.id.btnLeft && rightNumber<leftNumber) {
+            Toast.makeText(this, "Has ganado!", Toast.LENGTH_LONG).show()
+            result.text = "Has ganado!!"
+        }
+        else {
+            Toast.makeText(this, "Has perdido :(", Toast.LENGTH_LONG).show()
+            result.text = "Has perdido :("
+        }
+        assignNumbers()
     }
 
     fun rightClick(p: View) {
@@ -23,8 +49,7 @@ class MainActivity : AppCompatActivity() {
         else {
             Toast.makeText(this, "Has perdido :(", Toast.LENGTH_LONG).show()
         }
-        leftNumber = (0..100).random()
-        rightNumber = (0+Math.random()*100).toInt()
+        assignNumbers()
     }
 
     fun leftClick(p: View) {
@@ -35,8 +60,7 @@ class MainActivity : AppCompatActivity() {
         else {
             Toast.makeText(this, "Has perdido :(", Toast.LENGTH_LONG).show()
         }
-        leftNumber = (0..100).random()
-        rightNumber = (0+Math.random()*100).toInt()
+        assignNumbers()
     }
 }
 
